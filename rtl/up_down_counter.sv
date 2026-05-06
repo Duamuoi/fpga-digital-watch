db31ad030a51878c9ffb27b203d26fe6f7a07391
Duamuoi <nhannhin@student.unimelb.edu.au>
2026-05-06 07:43:24 +0000

    Fix width casting and counter logic

--- rtl/up_down_counter.sv
+++ rtl/up_down_counter.sv
@@ -1,39 +1,27 @@
-`timescale 1ns/1ps
+`timescale 1ns / 1ps
 
 module up_down_counter #(
-    parameter int MAX = 2,
+    parameter int MAX   = 2,
     parameter int WIDTH = 2
 ) (
-    input  logic clk,
-    input  logic enable,
-    input  logic up,
-    output logic [WIDTH-1:0] count
+    input logic clk,
+    input logic enable,
+    input logic up,
+    output logic [WIDTH-1:0] count = WIDTH'(0)
 );
 
-logic [WIDTH-1:0] next_count;
+  logic [WIDTH-1:0] next_count;
 
-initial count = '0;
+  always_ff @(posedge clk) if (enable) count <= next_count;
 
-always_ff @(posedge clk) begin
-    count <= next_count;
-end
-
-always_comb begin
-    next_count = count;
-
-    if (enable) begin
-        if (up) begin
-            if (count == MAX)
-                next_count = '0;
-            else
-                next_count = count + 1'b1;
-        end else begin
-            if (count == 0)
-                next_count = MAX[WIDTH-1:0];
-            else
-                next_count = count - 1'b1;
-        end
+  always_comb begin
+    if (up) begin
+      if (count == WIDTH'(MAX)) next_count = WIDTH'(0);
+      else next_count = count + 1'b1;
+    end else begin
+      if (count == WIDTH'(0)) next_count = WIDTH'(MAX);
+      else next_count = count - 1'b1;
     end
-end
+  end
 
 endmodule
