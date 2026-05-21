bb5bf1613bf0511a72ab7ed3d22a673017479f1d
Duamuoi <nhannhin@student.unimelb.edu.au>
2026-05-21 12:53:24 +0000

    rising edge detector

---
+++ rtl/rising_edge_detector.sv
@@ -0,0 +1,24 @@
+`timescale 1ns/1ps
+
+module rising_edge_detector (
+    input  logic clk,
+    input  logic sig_in,
+    output logic rise
+);
+
+    logic sig_prev;
+
+    // Store previous sampled value of sig_in
+    always_ff @(posedge clk) begin
+        sig_prev <= sig_in;
+    end
+
+    // Mealy output logic:
+    // rise goes high immediately when sig_in is high
+    // and the previously sampled value was low.
+    always_comb begin
+        rise = sig_in & ~sig_prev;
+    end
+
+endmodule
+
